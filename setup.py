f6b088bc4886d3fb2fa01c7847acf1566b01d2df
BitFloyd <sby.jcub@gmail.com>
2019-05-05 04:16:04 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='deepsbd',
+      version='0.1',
+      description='Subsample videos into shots',
+      url='http://github.com/BitFloyd/deepsbd',
+      author='Seby Jacob',
+      author_email='sby.jcub@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
