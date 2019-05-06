6f7caa4806db15a70e6e9e24cfacc2a739a18cde
BitFloyd <sby.jcub@gmail.com>
2019-05-06 16:05:44 -0400

    added decompose_video_to_shots_function

--- deepsbd/video_to_shots.py
+++ deepsbd/video_to_shots.py
@@ -3,7 +3,10 @@ from keras.models import load_model
 from config import n_frame_added,cut_detector,grad_detector,grad_n_frames_per_sample
 from read_video_cuboids import read_cuboid_from_video_cut_detection,get_SIM_for_grad_candidate
 from read_video_cuboids import get_frame_start_for_grad_cuboids
+import os
 import numpy as np
+from clockshortenstream.process_video_pkg.frame_writer import FrameWriter
+from clockshortenstream.process_video_pkg.frame_reader import Stream
 
 
 
@@ -74,5 +77,48 @@ class VideoToShots:
         self.full_trans = []
         self.full_trans.extend(self.cuts)
         self.full_trans.extend(self.grads)
+        self.full_trans.sort()
 
         return self.full_trans
+
+    def get_video_name_from_id(self,video_id):
+
+        return 'shot_'+str(video_id)+'.mp4'
+
+
+    def save_video_as_shots(self,out_path_for_video):
+
+        if not (os.path.exists(out_path_for_video)):
+            os.mkdir(out_path_for_video)
+
+        video_id = 0
+        video_name = self.get_video_name_from_id(video_id)
+
+        fReader = Stream(self.path_to_video,time_resolution=None)
+        frame = fReader.readNextFrameFromVideo()
+
+        fWriter = FrameWriter(out_path_for_video,video_name,frame_size=(frame.shape[0],frame.shape[1]),video_fps=fReader.frameReader.videoFPS)
+        fWriter.openVideoStream()
+
+        trans_to_write = list(self.full_trans)
+
+        trans_to_write.append(fReader.frameReader.numFrames)
+
+
+        while len(trans_to_write) > 0 and not fReader.videoFinished:
+            fWriter.writeFrameToVideo(frame)
+
+            if(fReader.frameReader.frameNumber ==trans_to_write[0]):
+                fWriter.closeVideoStream()
+                trans_to_write.pop(0)
+                if(not len(trans_to_write)>0):
+                    break
+                video_id+=1
+                video_name=self.get_video_name_from_id(video_id)
+                fWriter=FrameWriter(out_path_for_video,video_name,frame_size=(frame.shape[0],frame.shape[1]),video_fps=fReader.frameReader.videoFPS)
+                fWriter.openVideoStream()
+
+            frame = fReader.readNextFrameFromVideo()
+
+
+        return True
